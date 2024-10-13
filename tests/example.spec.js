da11e1a0883531d07ad1819779657c1d1830ad31
Hikutaatti <niko.muikku618@gmail.com>
2024-10-13 22:01:45 +0300

    added playwright and test

---
+++ tests/example.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('ScreenshotWeeklyMenu', async ({ page }) => {
+  await page.goto("https://mealdoo.com/week/uniresta/julinia/ravintolajulinia?date=2024-10-07&lang=fi&openAll=true&theme=light--light-green")
+  await page.getByRole('button', { name: 'Näytä kaikki päivät' }).click();
+  await page.waitForURL("https://mealdoo.com/week/uniresta/julinia/ravintolajulinia?date=2024-10-07&lang=fi&openAll=true&theme=light--light-green")
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });await expect(page.getByRole('button', { name: 'Näytä kaikki päivät' })).toBeVisible();
+});
